ad32e38f13a735a1f9debddec595c2728e11b977
yknomeh <yknomeh@protonmail.com>
2018-11-04 18:08:51 +0100

    gtk prototype

--- source/app.d
+++ source/app.d
@@ -1,39 +1,39 @@
 import std.stdio;
 import std.format;
+import core.stdc.stdlib;
 
 import gtk.Application : Application;
 import gio.Application : GioApplication = Application;
 import gtk.ApplicationWindow : ApplicationWindow;
 import gtkc.giotypes : GApplicationFlags;
+import gtk.Builder;
+import gtk.Button;
+import gtk.Window;
 
 import gtk.HeaderBar;
 import gtk.Label;
 
-class MainWindow : ApplicationWindow {
-  this(Application application) {
-    super(application);
-    initUI();
-    showAll();
-  }
-
-private void initUI() {
-    this.setSizeRequest(1024, 640);
-
-    //Titlebar
-    HeaderBar hb = new HeaderBar();
-    hb.setShowCloseButton(true);
-    hb.setTitle("Welcome");
-    this.setTitlebar(hb);
-
-    //Title
-		Label title = new Label("<span size=\"x-large\" weight=\"heavy\">I Love OS</span>");
-    title.setUseMarkup(true);
-    add(title);
-  }
-}
+immutable string GLADE_MAIN = "source/ui/main.glade";
 
 int main(string[] args) {
-    auto application = new Application("love.os.greeter", GApplicationFlags.FLAGS_NONE);
-    application.addOnActivate(delegate void(GioApplication app) { MainWindow mainWindow = new MainWindow(application); });
-    return application.run(args);
+  auto application = new Application("love.os.greeter", GApplicationFlags.FLAGS_NONE);
+
+  void mainPage(GioApplication a) {
+    auto builder = new Builder();
+    if (!builder.addFromFile(GLADE_MAIN)) {
+      writeln("File does not exist.");
+      exit(1);
+    }
+    auto window = cast(ApplicationWindow)builder.getObject("window");
+    window.setApplication(application);
+    if (window !is null) {
+      window.setTitle("Welcome!");
+    } else {
+      writeln("No love from gtk ;(");
+      exit(1);
+    }
+	}
+
+application.addOnActivate(&mainPage);
+  return application.run(args);
 }
